2d9645412e1acae5f48e2331a33e9df6fad89aeb
Josue Villasante <javillasantet@gmail.com>
2017-09-03 02:25:49 -0500

    added node version and Procfile

--- main.js
+++ main.js
@@ -15,6 +15,6 @@ app.set('view engine', 'njk')
 
 config(app, controllers)
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Example app listening on port ${process.env.PORT || 3000}!`)
 })
